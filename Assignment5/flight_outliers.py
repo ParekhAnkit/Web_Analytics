bb01268ea83fb8052de9589793e23f9af6207395
ParekhAnkit <aparekh3@stevens.edu>
2017-04-13 17:56:22 -0400

    Update flight_outliers.py

--- Assignment5/flight_outliers.py
+++ Assignment5/flight_outliers.py
@@ -26,13 +26,14 @@ class DataFrame(object):
             from_input_actions.send_keys(from_place)
             from_input_actions.send_keys(Keys.ENTER)
             from_input_actions.perform()
-
+            time.sleep(0.5)
             to_input = driver.find_element_by_xpath('//*[@id="root"]/div[3]/div[3]/div/div[4]/div/div')
             to_input.click()
             to_input_actions = ActionChains(driver)
             to_input_actions.send_keys(city_name)
             to_input_actions.send_keys(Keys.ENTER)
             to_input_actions.perform()
+            time.sleep(0.5)
 
 
             url=driver.current_url
@@ -43,22 +44,26 @@ class DataFrame(object):
             new_url=c_url.replace(d[-1],"d="+start_date)
             #print new_url
             driver.get(new_url)
+            time.sleep(0.5)
 
             results = driver.find_elements_by_class_name('LJTSM3-v-d')
             test = results[0]
             bars = test.find_elements_by_class_name('LJTSM3-w-x')
             data = []
+            time.sleep(0.5)
 
             for bar in bars:
                 ActionChains(driver).move_to_element(bar).perform()
                 time.sleep(0.001)
                 data.append((test.find_element_by_class_name('LJTSM3-w-k').find_elements_by_tag_name('div')[0].text,
                 test.find_element_by_class_name('LJTSM3-w-k').find_elements_by_tag_name('div')[1].text))
+            time.sleep(0.5)
 
 
             df=pd.DataFrame(data)
             df.columns=['Price','Departure - Return Date']
             print df
+            time.sleep(0.5)
 
     def scrape_data_90(start_date, from_place, to_place, city_name):
             t= str(start_date)
@@ -76,6 +81,7 @@ class DataFrame(object):
             from_input_actions.send_keys(from_place)
             from_input_actions.send_keys(Keys.ENTER)
             from_input_actions.perform()
+            time.sleep(0.5)
 
             to_input = driver.find_element_by_xpath('//*[@id="root"]/div[3]/div[3]/div/div[4]/div/div')
             to_input.click()
@@ -83,6 +89,7 @@ class DataFrame(object):
             to_input_actions.send_keys(city_name)
             to_input_actions.send_keys(Keys.ENTER)
             to_input_actions.perform()
+            time.sleep(0.5)
 
 
             url=driver.current_url
@@ -93,17 +100,19 @@ class DataFrame(object):
             new_url=c_url.replace(d[-1], date)
             #print new_url
             driver.get(new_url)
+            time.sleep(0.5)
 
             results = driver.find_elements_by_class_name('LJTSM3-v-d')
             test = results[0]
             bars = test.find_elements_by_class_name('LJTSM3-w-x')
             data = []
+            time.sleep(0.5)
 
             for bar in bars:
                 ActionChains(driver).move_to_element(bar).perform()
                 time.sleep(0.001)
                 data.append((test.find_element_by_class_name('LJTSM3-w-k').find_elements_by_tag_name('div')[0].text, test.find_element_by_class_name('LJTSM3-w-k').find_elements_by_tag_name('div')[1].text))
-
+            time.sleep(0.5)
 
             df=pd.DataFrame(data)
             driver.get(new_url)
@@ -111,22 +120,26 @@ class DataFrame(object):
             page_for30_90days = driver.find_element_by_xpath('//*[@id="root"]/div[3]/div[4]/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div[5]/div')
             page_for30_90days.click()
             data_for30_90days = []
+            time.sleep(0.5)
 
 
             results = driver.find_elements_by_class_name('LJTSM3-v-d')
             test = results[0]
             bars = test.find_elements_by_class_name('LJTSM3-w-x')
+            time.sleep(0.5)
 
             for bar in bars:
                 ActionChains(driver).move_to_element(bar).perform()
                 time.sleep(0.001)
                 data_for30_90days.append((test.find_element_by_class_name('LJTSM3-w-k').find_elements_by_tag_name('div')[0].text, test.find_element_by_class_name('LJTSM3-w-k').find_elements_by_tag_name('div')[1].text))
+            time.sleep(0.5)
 
             data_for60_90days = data_for30_90days[30:60]
             df = df.append(data_for60_90days)
             df.columns=['Price', 'Departure - Return Date']
             df.index = range(90)
             print df
+
 
     def task_3_dbscan(self, flight_data):
         flight_data['Price'] = flight_data['Price'].map(lambda x: re.sub("[^\d\.]", "", x))
